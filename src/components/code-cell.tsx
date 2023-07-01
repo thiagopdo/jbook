0d55ade1b77173bc7c33c50cdf6a18473a1c37c5
thiagopdo <thiago.falconer@gmail.com>
2023-07-01 05:14:36 -0300

    updated show builtin function react pre imported

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -20,11 +20,22 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
 
     const cumulativeCode = [
       ` 
+        import React from 'react';
+        import ReactDOM from 'react-dom';
+        import { createRoot } from "react-dom/client";
+
+
         const show = (value) => {
+          const root = document.querySelector('#root');
+
           if (typeof value === "object") {
-            document.querySelector("#root").innerHTML = JSON.stringify(value);
+            if (value.$$typeof && value.props) {
+              ReactDOM.render(value, root);
+            } else {
+              root.innerHTML = JSON.stringify(value);
+            }
           } else {
-            document.querySelector('#root').innerHTML = value;
+            root.innerHTML = value;
           }
         };
       `,
